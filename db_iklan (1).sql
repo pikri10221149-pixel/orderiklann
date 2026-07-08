bfe6f45b702592ccd5079758f6942f29116efb46
pikri10221149-pixel <pikri10221149@student.sttcipasung.ac.id>
2026-07-08 11:57:55 +0700

    Add files via upload

---
+++ db_iklan (1).sql
@@ -0,0 +1,296 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 01, 2024 at 04:37 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_iklan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `nama_barang` text NOT NULL,
+  `merk` varchar(255) NOT NULL,
+  `ukuran` varchar(255) NOT NULL,
+  `materi` varchar(255) NOT NULL,
+  `harga_beli` varchar(255) NOT NULL,
+  `harga_jual` varchar(255) NOT NULL,
+  `diskon` int(11) NOT NULL,
+  `ppn` int(11) NOT NULL,
+  `satuan_barang` varchar(255) NOT NULL,
+  `stok` text NOT NULL,
+  `tgl_input` varchar(255) NOT NULL,
+  `tgl_update` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `id_barang`, `id_kategori`, `nama_barang`, `merk`, `ukuran`, `materi`, `harga_beli`, `harga_jual`, `diskon`, `ppn`, `satuan_barang`, `stok`, `tgl_input`, `tgl_update`) VALUES
+(8, 'BR001', 8, 'dinda', '081353534356', '6M', 'Halus', '', '100000', 1000, 100, '', '8', '29 September 2024, 16:15', '30 September 2024, 10:37'),
+(10, 'BR002', 9, 'zahra anisa ', '082256958475', 'M', 'Halus', '', '5000', 500, 0, '', '9', '29 September 2024, 16:37', '29 September 2024, 20:55'),
+(11, 'BR003', 9, 'aisa', '082256958475', 'M', 'Halus', '', '100000', 1000, 1000, '', '7', '29 September 2024, 16:52', '29 September 2024, 17:00'),
+(12, 'BR004', 8, 'dinda', '082256958475', 'XL', 'Halus', '', '50000', 1000, 100, '', '5', '29 September 2024, 17:00', '30 September 2024, 10:36'),
+(13, 'BR005', 9, 'amar', '082256958475', '10 s', 'Halus', '', '70000', 5000, 1000, '', '10', '30 September 2024, 14:50', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL,
+  `tgl_input` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `tgl_input`) VALUES
+(8, 'Baris', '29 September 2024, 15:50'),
+(9, 'Kolom', '29 September 2024, 15:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id_login` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `pass` char(32) NOT NULL,
+  `id_member` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id_login`, `user`, `pass`, `id_member`) VALUES
+(1, 'admin', '202cb962ac59075b964b07152d234b70', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id_member` int(11) NOT NULL,
+  `nm_member` varchar(255) NOT NULL,
+  `alamat_member` text NOT NULL,
+  `telepon` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gambar` text NOT NULL,
+  `NIK` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id_member`, `nm_member`, `alamat_member`, `telepon`, `email`, `gambar`, `NIK`) VALUES
+(1, 'PIKRI APANDI HUSNI', 'CIHIDENG', '081234567890', 'example@gmail.com', '1727606793Logo Radar Tasik id putih inspirasi untuk semua (1).png', '12314121');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nota`
+--
+
+CREATE TABLE `nota` (
+  `id_nota` int(11) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `jumlah` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `tanggal_input` varchar(255) NOT NULL,
+  `periode` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `nota`
+--
+
+INSERT INTO `nota` (`id_nota`, `id_barang`, `id_member`, `jumlah`, `total`, `tanggal_input`, `periode`) VALUES
+(3, 'BR001', 1, '1', '111', '29 September 2024, 17:45', '09-2024'),
+(4, 'BR002', 1, '1', '5000', '29 September 2024, 17:45', '09-2024'),
+(5, 'BR003', 1, '1', '100000', '29 September 2024, 17:45', '09-2024'),
+(6, 'BR001', 1, '1', '111', '29 September 2024, 17:45', '09-2024'),
+(7, 'BR002', 1, '1', '5000', '29 September 2024, 17:45', '09-2024'),
+(8, 'BR003', 1, '1', '100000', '29 September 2024, 17:45', '09-2024'),
+(9, 'BR003', 1, '1', '100000', '29 September 2024, 18:05', '09-2024'),
+(10, 'BR002', 1, '1', '5000', '29 September 2024, 20:52', '09-2024'),
+(11, 'BR002', 1, '3', '15000', '29 September 2024, 20:52', '09-2024'),
+(12, 'BR002', 1, '3', '15000', '29 September 2024, 20:52', '09-2024'),
+(13, 'BR004', 1, '1', '50000', '30 September 2024, 10:02', '09-2024'),
+(14, 'BR001', 1, '1', '100000', '30 September 2024, 11:28', '09-2024'),
+(15, 'BR004', 1, '1', '50000', '30 September 2024, 11:28', '09-2024');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `jumlah` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `tanggal_input` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `id_barang`, `id_member`, `jumlah`, `total`, `tanggal_input`) VALUES
+(11, 'BR001', 1, '1', '100000', '30 September 2024, 11:28'),
+(12, 'BR004', 1, '1', '50000', '30 September 2024, 11:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `toko`
+--
+
+CREATE TABLE `toko` (
+  `id_toko` int(11) NOT NULL,
+  `nama_toko` varchar(255) NOT NULL,
+  `alamat_toko` text NOT NULL,
+  `tlp` varchar(255) NOT NULL,
+  `nama_pemilik` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `toko`
+--
+
+INSERT INTO `toko` (`id_toko`, `nama_toko`, `alamat_toko`, `tlp`, `nama_pemilik`) VALUES
+(1, 'Aplikasi Order Iklan', 'Radar, Tasikmalaya', '081234567890', 'PIKRI APANDI HUSNI');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indexes for table `nota`
+--
+ALTER TABLE `nota`
+  ADD PRIMARY KEY (`id_nota`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`);
+
+--
+-- Indexes for table `toko`
+--
+ALTER TABLE `toko`
+  ADD PRIMARY KEY (`id_toko`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `nota`
+--
+ALTER TABLE `nota`
+  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `toko`
+--
+ALTER TABLE `toko`
+  MODIFY `id_toko` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ tambah.php
@@ -0,0 +1,83 @@
+<?php
+
+session_start();
+if (!empty($_SESSION['admin'])) {
+    require '../../config.php';
+    if (!empty($_GET['kategori'])) {
+        $nama= htmlentities(htmlentities($_POST['kategori']));
+        $tgl= date("j F Y, G:i");
+        $data[] = $nama;
+        $data[] = $tgl;
+        $sql = 'INSERT INTO kategori (nama_kategori,tgl_input) VALUES(?,?)';
+        $row = $config -> prepare($sql);
+        $row -> execute($data);
+        echo '<script>window.location="../../index.php?page=kategori&&success=tambah-data"</script>';
+    }
+
+    if (!empty($_GET['barang'])) {
+        $id = htmlentities($_POST['id']);
+        $kategori = htmlentities($_POST['kategori']);
+        $nama = htmlentities($_POST['nama']);
+        $merk = htmlentities($_POST['merk']);
+        $ukuran = htmlentities($_POST['ukuran']);
+        $materi = htmlentities($_POST['materi']);
+        $beli = htmlentities($_POST['beli']);
+        $jual = htmlentities($_POST['jual']);
+        $diskon = htmlentities($_POST['diskon']);
+        $ppn = htmlentities($_POST['ppn']);
+        $satuan = htmlentities($_POST['satuan']);
+        $stok = htmlentities($_POST['stok']);
+        $tgl = htmlentities($_POST['tgl']);
+
+        $data[] = $id;
+        $data[] = $kategori;
+        $data[] = $nama;
+        $data[] = $merk;
+        $data[] = $ukuran;
+        $data[] = $materi;
+        $data[] = $beli;
+        $data[] = $jual;
+        $data[] = $diskon;
+        $data[] = $ppn;
+        $data[] = $satuan;
+        $data[] = $stok;
+        $data[] = $tgl;
+        $sql = 'INSERT INTO barang (id_barang,id_kategori,nama_barang,merk,ukuran,materi,harga_beli,harga_jual,diskon,ppn,satuan_barang,stok,tgl_input) 
+			    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?) ';
+        $row = $config -> prepare($sql);
+        $row -> execute($data);
+        echo '<script>window.location="../../index.php?page=barang&success=tambah-data"</script>';
+    }
+    
+    if (!empty($_GET['jual'])) {
+        $id = $_GET['id'];
+
+        // get tabel barang id_barang
+        $sql = 'SELECT * FROM barang WHERE id_barang = ?';
+        $row = $config->prepare($sql);
+        $row->execute(array($id));
+        $hsl = $row->fetch();
+
+        if ($hsl['stok'] > 0) {
+            $kasir =  $_GET['id_kasir'];
+            $jumlah = 1;
+            $total = $hsl['harga_jual'];
+            $tgl = date("j F Y, G:i");
+
+            $data1[] = $id;
+            $data1[] = $kasir;
+            $data1[] = $jumlah;
+            $data1[] = $total;
+            $data1[] = $tgl;
+
+            $sql1 = 'INSERT INTO penjualan (id_barang,id_member,jumlah,total,tanggal_input) VALUES (?,?,?,?,?)';
+            $row1 = $config -> prepare($sql1);
+            $row1 -> execute($data1);
+
+            echo '<script>window.location="../../index.php?page=jual&success=tambah-data"</script>';
+        } else {
+            echo '<script>alert("Stok Barang Anda Telah Habis !");
+					window.location="../../index.php?page=jual#keranjang"</script>';
+        }
+    }
+}
